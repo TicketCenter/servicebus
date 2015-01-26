f63e60c7b65e5363d3d64c0d7650230e0c6f1712
Nils Berlijn <n_berlijn@hotmail.com>
2015-01-26 20:23:10 +0100

    Database default settings removed.

--- servicebus/settings.py
+++ servicebus/settings.py
@@ -57,10 +57,10 @@ WSGI_APPLICATION = 'servicebus.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 # Internationalization
